f44c8c0146f05ec28ab7b5032e7511115b83af78
Gift <laurandidi21@gmailcom>
2019-03-02 07:32:36 +0100

    Add disqus to site

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,57 +1,56 @@
 <template>
   <Layout>
     <div class="post-title">
-      <h1 class="post-title__text">
-        {{ $page.post.title }}
-      </h1>
-      
-      <PostMeta :post="$page.post" />
+      <h1 class="post-title__text">{{ $page.post.title }}</h1>
 
+      <PostMeta :post="$page.post"/>
     </div>
-    
+
     <div class="post content-box">
       <div class="post__header">
-        <g-image alt="Cover image" v-if="$page.post.coverImage" :src="$page.post.coverImage" />
+        <g-image alt="Cover image" v-if="$page.post.coverImage" :src="$page.post.coverImage"/>
       </div>
 
-      <div class="post__content" v-html="$page.post.content" />
+      <div class="post__content" v-html="$page.post.content"/>
 
       <div class="post__footer">
-        <PostTags :post="$page.post" />
+        <PostTags :post="$page.post"/>
+      </div>
+      <div class="post-comments">
+        <!-- Add comment widgets here -->
+        <Comment/>
       </div>
     </div>
 
-    <div class="post-comments">
-      <!-- Add comment widgets here -->
-    </div>
-
-    <Author class="post-author" />
+    <Author class="post-author"/>
   </Layout>
 </template>
 
 <script>
-import PostMeta from '~/components/PostMeta'
-import PostTags from '~/components/PostTags'
-import Author from '~/components/Author.vue'
+import PostMeta from "~/components/PostMeta";
+import PostTags from "~/components/PostTags";
+import Author from "~/components/Author.vue";
+import Comment from "~/components/Comment.vue";
 
 export default {
   components: {
     Author,
     PostMeta,
-    PostTags
+    PostTags,
+    Comment
   },
-  metaInfo () {
+  metaInfo() {
     return {
       title: this.$page.post.title,
       meta: [
         {
-          name: 'description',
+          name: "description",
           content: this.$page.post.description
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <page-query>
@@ -79,7 +78,6 @@ query Post ($path: String!) {
 }
 
 .post {
-
   &__header {
     width: calc(100% + var(--space) * 2);
     margin-left: calc(var(--space) * -1);
@@ -87,7 +85,7 @@ query Post ($path: String!) {
     margin-bottom: calc(var(--space) / 2);
     overflow: hidden;
     border-radius: var(--radius) var(--radius) 0 0;
-    
+
     img {
       width: 100%;
     }
@@ -118,7 +116,7 @@ query Post ($path: String!) {
 
 .post-comments {
   padding: calc(var(--space) / 2);
-  
+
   &:empty {
     display: none;
   }
