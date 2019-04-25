16010559d7a76b1cfffb211173f770ddf10b724a
Gift <laurandidi21@gmailcom>
2019-04-25 18:21:27 +0100

    Fix tags alignment

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="post-card content-box" :class="{'post-card--has-poster' : post.poster}">
     <div class="post-card__header">
-      <g-image alt="Cover image" v-if="post.coverImage" class="post-card__image" :src="post.coverImage" />
+      <g-image
+        alt="Cover image"
+        v-if="post.coverImage"
+        class="post-card__image"
+        :src="post.coverImage"
+      />
     </div>
     <div class="post-card__content">
-      <h2 class="post-card__title" v-html="post.title" />
-      <p class="post-card__description" v-html="post.description" />
-      
-      <PostMeta class="post-card__meta" :post="post" />
-      <PostTags class="post-card__tags" :post="post" />
+      <h2 class="post-card__title" v-html="post.title"/>
+      <p class="post-card__description" v-html="post.description"/>
+
+      <PostMeta class="post-card__meta" :post="post"/>
+      <PostTags class="post-card__tags" :post="post"/>
 
       <g-link class="post-card__link" :to="post.path">Link</g-link>
     </div>
@@ -16,16 +21,16 @@
 </template>
 
 <script>
-import PostMeta from '~/components/PostMeta'
-import PostTags from '~/components/PostTags'
+import PostMeta from "~/components/PostMeta";
+import PostTags from "~/components/PostTags";
 
 export default {
   components: {
     PostMeta,
     PostTags
   },
-  props: ['post'],
-}
+  props: ["post"]
+};
 </script>
 
 <style lang="scss">
@@ -56,7 +61,7 @@ export default {
 
   &:hover {
     transform: translateY(-5px);
-    box-shadow: 1px 10px 30px 0 rgba(0,0,0,.1);
+    box-shadow: 1px 10px 30px 0 rgba(0, 0, 0, 0.1);
   }
 
   &__tags {
@@ -70,7 +75,7 @@ export default {
     left: 0;
     width: 100%;
     height: 100%;
-    opacity: 0.0;
+    opacity: 0;
     overflow: hidden;
     text-indent: -9999px;
     z-index: 0;
