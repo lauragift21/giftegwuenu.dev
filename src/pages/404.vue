3981b7ecf720331b17ede36044d802cfb14935b0
Gift <laurandidi21@gmailcom>
2019-05-21 09:05:36 +0100

    Add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <g-link to="/">Go Home</g-link>
+    <g-image src="../assets/images/404.svg"></g-image>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
