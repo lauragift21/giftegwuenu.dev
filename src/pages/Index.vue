dbfed8a8e0e40ad5fbb04034181ad225f599ba0c
lauragift21 <laurandidi21@gmail.com>
2019-10-21 15:37:50 +0100

    Add carbon ads

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,15 @@
 <template>
   <Layout :show-logo="false">
     <!-- Author intro -->
-    <Author :show-title="true"/>
+    <Author :show-title="true" />
     <!-- List posts -->
     <div id="main" class="posts">
-      <div id="codefund"></div>
+      <div id="carbonads"></div>
       <div class="posts-card">
-        <PostCard v-for="edge in $page.allPost.edges" :key="edge.node.id" :post="edge.node"/>
+        <PostCard v-for="edge in $page.allPost.edges" :key="edge.node.id" :post="edge.node" />
       </div>
     </div>
-    <Pager :info="$page.allPost.pageInfo"/>
+    <Pager :info="$page.allPost.pageInfo" />
   </Layout>
 </template>
 
@@ -59,7 +59,8 @@ export default {
     title: "Home",
     script: [
       {
-        src: "https://codefund.app/properties/264/funder.js?template=square",
+        src:
+          "https://cdn.carbonads.com/carbon.js?serve=CE7DV2J7&placement=wwwgiftegwuenucom",
         async: true
       }
     ]
@@ -90,7 +91,74 @@ a:hover {
   grid-template-columns: 22% 60%;
 }
 
-#codefund {
+#carbonads {
+  position: relative;
+  display: flex;
+  width: 300px;
+  height: 250px;
+  background-color: #fff;
+  box-shadow: inset 0 0 1px 1px hsla(0, 0%, 0%, 0.15);
+  text-align: center;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Helvetica, Arial,
+    sans-serif;
+
+  justify-content: center;
+  align-items: center;
+}
+
+#carbonads a {
+  color: #111;
+  text-decoration: none;
+}
+
+#carbonads a:hover {
+  color: #111;
+}
+
+#carbonads span {
+  display: block;
+  overflow: hidden;
+}
+
+.carbon-img {
+  display: block;
+  margin: 0 0 8px;
+  line-height: 1;
+}
+
+.carbon-img img {
+  max-width: 150px !important;
+  width: 150px;
+  height: auto;
+}
+
+.carbon-text {
+  display: block;
+  margin-bottom: 8px;
+  padding: 0 10px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1.35;
+}
+
+.carbon-poweredby {
+  position: absolute;
+  right: 10px;
+  bottom: 10px;
+  display: block;
+  padding: 5px 6px;
+  border-radius: 2.5px;
+  background-color: hsl(246, 93%, 69%);
+  color: #fff !important;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+  font-weight: 600;
+  font-size: 8px;
+  line-height: 1;
+}
+
+#carbonads {
   margin-right: 0.7em;
 }
 
@@ -98,13 +166,13 @@ a:hover {
   #main {
     display: block;
   }
-  #codefund {
+  #carbonads {
     display: none;
   }
 }
 
 @media only screen and (min-width: 1650px) {
-  #codefund {
+  #carbonads {
     margin-right: -9em;
   }
 }
--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -6,7 +6,7 @@
     </div>
 
     <div class="post__flex">
-      <div id="codefund"></div>
+      <div id="carbonads"></div>
       <div class="post content-boxs">
         <div class="post__header">
           <g-image alt="Cover image" v-if="$page.post.coverImage" :src="$page.post.coverImage"/>
@@ -62,7 +62,7 @@ export default {
       script: [
         {
           src:
-            "https://codefund.app/properties/264/funder.js/?template=image-only&theme=light",
+            "https://cdn.carbonads.com/carbon.js?serve=CE7DV2J7&placement=wwwgiftegwuenucom",
           async: true
         }
       ]
@@ -153,6 +153,73 @@ query Post ($path: String!) {
   padding: 0;
 }
 
+#carbonads {
+  position: relative;
+  display: flex;
+  width: 300px;
+  height: 250px;
+  background-color: #fff;
+  box-shadow: inset 0 0 1px 1px hsla(0, 0%, 0%, 0.15);
+  text-align: center;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Helvetica, Arial,
+    sans-serif;
+
+  justify-content: center;
+  align-items: center;
+}
+
+#carbonads a {
+  color: #111;
+  text-decoration: none;
+}
+
+#carbonads a:hover {
+  color: #111;
+}
+
+#carbonads span {
+  display: block;
+  overflow: hidden;
+}
+
+.carbon-img {
+  display: block;
+  margin: 0 0 8px;
+  line-height: 1;
+}
+
+.carbon-img img {
+  max-width: 150px !important;
+  width: 150px;
+  height: auto;
+}
+
+.carbon-text {
+  display: block;
+  margin-bottom: 8px;
+  padding: 0 10px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1.35;
+}
+
+.carbon-poweredby {
+  position: absolute;
+  right: 10px;
+  bottom: 10px;
+  display: block;
+  padding: 5px 6px;
+  border-radius: 2.5px;
+  background-color: hsl(246, 93%, 69%);
+  color: #fff !important;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+  font-weight: 600;
+  font-size: 8px;
+  line-height: 1;
+}
+
 @media only screen and (min-width: 320px) and (max-width: 800px) {
   .post {
     &__flex {
@@ -163,13 +230,16 @@ query Post ($path: String!) {
   .content-boxs {
     margin: 0 !important;
   }
+  #carbonads {
+    text-align: center;
+  }
 }
 
 @media only screen and (min-width: 1650px) {
   .content-boxs {
     margin-left: 6em;
   }
-  #codefund {
+  #carbonads {
     margin-right: -9em;
   }
 }
