ed5ac45e99f71c47e5b2fb62435c6d28216494bb
Gift Egwuenu <laurandidi21@gmail.com>
2020-05-09 23:24:58 +0100

    upgrade-to-version-0.7 2

--- src/components/Latest.vue
+++ src/components/Latest.vue
@@ -3,9 +3,11 @@
   <h2 class="text-4xl xs:text-xl text-left  font-semibold mb-5 sm:text-center sm:text-3xl">Latest Articles</h2>
   <div class="pr-8 sm:ml-3 sm:mx-auto">
     <div class="card rounded-sm border border-l-8 shadow-md my-4 pb-3" v-for="edge in $static.allPost.edges" :key="edge.node.id">
-      <h3 class="text-md px-5 py-2 font-semibold">{{edge.node.date}}</h3>
-      <h3 class="text-xl px-5 py-2 font-semibold">{{edge.node.title}}</h3>
-      <h3 class="description text-md mb-3 px-5 py-2">{{edge.node.description}}</h3>
+      <g-link :to="edge.node.path">
+        <h3 class="text-md px-5 py-2 font-semibold">{{edge.node.date}}</h3>
+        <h3 class="text-xl px-5 py-2 font-semibold">{{edge.node.title}}</h3>
+        <h3 class="description text-md mb-3 px-5 py-2">{{edge.node.description}}</h3>
+      </g-link>
     </div>
   </div>
  </section>
