b7c1ff2df5b6bb04fd5085e218d75882003121d3
Gift Egwuenu <laurandidi21@gmail.com>
2020-05-21 12:02:11 +0100

    update about

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,8 @@
     <h2 class="text-5xl sm:text-4xl  font-bold sm:px-5 sm:text-center">A bit about me</h2>
     <p class="text-2xl font-medium pt-0 w-11/12 mt-6 sm:mt-1 sm:text-center leading-loose sm:pt-5 sm:mx-auto">
       I’m passionate about making the web accessible to everyone and also an advocate for building open-source projects
-      and I enjoy solving complex problems using web technologies. I’m passionate about building communities which led me to start the Vue Vixens chapter in Nigeria and the JAMstack community in Lagos. When I’m not coding you can find me geeking about photography and exploring different ways of documenting stories using photography.
+      and I enjoy solving complex problems using web technologies. 
+      I’m also a big advocate for building open and diverse communities which led me to start the Vue Vixens chapter in Nigeria and the JAMstack community in Lagos. I'm a co-organizer of the Concatenate Conference, a Media Developer Expert at Cloudinary, and an Auth0 Ambassador. When I'm not coding you can find me geeking about photography and exploring different ways of documenting stories using photography and cooking delicious delicacies.
     </p>
   </section>
 </template>
