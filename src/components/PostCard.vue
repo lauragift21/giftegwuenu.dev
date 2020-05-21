cf32307e1193fdc2262db44292392380ff7bb354
Gift Egwuenu <laurandidi21@gmail.com>
2020-05-21 11:55:39 +0100

    [fix][s] - remove redundant read more

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="post-card rounded-md mx-10 sm:mx-4 px-8 py-6 transform hover:-translate-y-1" :class="{'post-card--has-poster' : post.poster}">
+  <div class="post-card rounded-md mx-4 sm:mx-4 px-8 py-6 transform hover:-translate-y-1" :class="{'post-card--has-poster' : post.poster}">
     <g-link class="font-medium" :to="post.path">
       <h1 class="text-xl font-extrabold" v-html="post.title" />
       <p class="text-base font-medium py-3" v-html="post.description" />
       <PostMeta class="font-bold mb-4" :post="post" />
-      <g-link class="font-medium flex justify-end" :to="post.path">
+      <!-- <g-link class="font-medium flex justify-end" :to="post.path">
           <span class="bg-pink-600 text-black hover:font-semibold rounded px-2 py-1">Read More</span>
-      </g-link>
+      </g-link> -->
     </g-link>
   </div>
 </template>
