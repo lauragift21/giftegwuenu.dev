c10c90fdfb24b8f26cc5d5fa17def0e35b3473eb
Gift Egwuenu <laurandidi21@gmail.com>
2020-11-06 11:28:08 +0100

    remove change

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="about md:px-6 my-10 sm:my-6" id="about" data-aos="fade">
-    <h2 class="text-5xl sm:text-4xl  font-bold sm:px-5 sm:text-center">A lil bit about me</h2>
+    <h2 class="text-5xl sm:text-4xl  font-bold sm:px-5 sm:text-center">A bit about me</h2>
     <p class="text-2xl font-medium pt-0 w-11/12 mt-6 sm:mt-1 sm:text-center leading-loose sm:pt-5 sm:mx-auto">
       I’m passionate about making the web accessible to everyone and also an advocate for building open-source projects
       and I enjoy solving complex problems using web technologies. 
