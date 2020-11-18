81bf98e6263a47578d26059c8f7cafa425680325
Gift Egwuenu <laurandidi21@gmail.com>
2020-11-18 15:35:45 +0100

    add buymecoffee button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,49 @@
 <template>
   <footer class="pt-10 xs:pt-5" id="contact" data-aos="fade">
     <h3 class="text-5xl font-bold pb-4">Get in Touch</h3>
-    <div class="box w-1/2 mx-auto py-8 border-solid border-4 rounded-lg sm:w-10/12">
-      <p class="font-bold text-2xl pb-16 sm:pb-8">Want to work together or have any questions?</p>
+    <div
+      class="box w-1/2 mx-auto py-8 border-solid border-4 rounded-lg sm:w-10/12"
+    >
+      <p class="font-bold text-2xl pb-16 sm:pb-8">
+        Want to work together or have any questions?
+      </p>
       <a
         class="mail hover:shadow font-bold py-3 px-8 text-xl rounded text-center"
         href="mailto:laurandidi21@gmail.com"
-      >Say Hello 👋</a>
+        >Say Hello 👋</a
+      >
     </div>
 
     <div class="px-4 pb-4 sm:block">
       <div class="my-4 font-bold">
-        <a href="https://patreon.com/lauragift21" target="_blank" rel="noopener" class="px-3 text-xl">Patreon</a>
-        <a href="resume.pdf" target="_blank" rel="noopener" class="px-3 text-xl">Resume</a>
-        <a href="https://github.com/lauragift21" target="_blank" rel="noopener" class="px-3 text-xl">GitHub</a>
+        <a
+          href="https://patreon.com/lauragift21"
+          target="_blank"
+          rel="noopener"
+          class="px-3 text-xl"
+          >Patreon</a
+        >
+        <a href="resume.pdf" target="_blank" rel="noopener" class="px-3 text-xl"
+          >Resume</a
+        >
+        <a
+          href="https://github.com/lauragift21"
+          target="_blank"
+          rel="noopener"
+          class="px-3 text-xl"
+          >GitHub</a
+        >
       </div>
-      <p class="font-bold text-lg col-end-3">Designed and Developed by <span class="footnote py-1 px-2 rounded-sm">Gift Egwuenu</span></p>
+      <div class="flex my-4 justify-center">
+        <a href="https://www.buymeacoffee.com/lauragift"
+          ><img
+            src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=lauragift&button_colour=e2216a&font_colour=ffffff&font_family=Cookie&outline_colour=ffffff&coffee_colour=FFDD00"
+        /></a>
+      </div>
+      <p class="font-bold text-lg col-end-3">
+        Designed and Developed by
+        <span class="footnote py-1 px-2 rounded-sm">Gift Egwuenu</span>
+      </p>
     </div>
   </footer>
 </template>
