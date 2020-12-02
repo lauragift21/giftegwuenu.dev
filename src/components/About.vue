9e0b5050388e4d3d681b622133490b2b692e94ec
Gift Egwuenu <laurandidi21@gmail.com>
2020-12-02 11:02:58 +0100

    add new profile image

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,8 +2,8 @@
   <section class="about md:px-6 my-10 sm:my-6" id="about" data-aos="fade">
     <h2 class="text-5xl sm:text-4xl  font-bold sm:px-5 sm:text-center">A bit about me</h2>
     <p class="text-2xl font-medium pt-0 w-11/12 mt-6 sm:mt-1 sm:text-center leading-loose sm:pt-5 sm:mx-auto">
-      I'm a Frontend Engineer passionate about making the web accessible to everyone & an advocate for building developer communities. I enjoy creating content this includes technical tutorials, video tutorials, and content about my life as a developer. 
-      I'm a Media Developer Expert at Cloudinary, and an Auth0 Ambassador and Nuxt.js Ambassador. When I'm not coding you can find me geeking about photography and exploring different ways of documenting stories using photography and cooking delicious delicacies.
+      I'm a frontend developer passionate about making the web accessible to everyone and an advocate for building open and inclusive developer communities. I love sharing my knowledge with others and I do that through content creation on my Blog and YouTube channel. 
+      I'm a Media Developer Expert at Cloudinary, an Auth0 Ambassador, and Nuxt.js Ambassador. When I'm not coding you can find me geeking about photography and exploring different ways of documenting stories using photography.
     </p>
   </section>
 </template>
