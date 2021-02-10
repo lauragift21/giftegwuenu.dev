e5ce1898d76762a75d6165bc08033dcebc62bd7b
Gift Egwuenu <gift.egwuenu@valtech.com>
2021-02-10 09:42:16 +0100

    Add ConvertKit Newsletter

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -1,59 +1,20 @@
 <template>
-  <div>
-    <h2 class="text-2xl text-center">Subscribe to my Newsletter</h2>
-    <p
-      class="w-3/4 mx-auto text-center pb-3 text-xl sm:text-lg"
-    >Don't miss out on new articles from me. Get notified when a new article is published.</p>
-    <form
-      class="form"
-      action="https://tinyletter.com/lauragift21"
-      method="post"
-      target="popupwindow"
-      onsubmit="window.open('https://tinyletter.com/lauragift21', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true"
-    >
-      <input
-        type="text"
-        name="email"
-        class="form__email"
-        id="email"
-        onfocus="this.value=''"
-        aria-label="email"
-        placeholder="Email address"
-      >
-      <input type="hidden" value="1" name="embed">
-      <input class="form__submit" type="submit" value="Subscribe">
-    </form>
-  </div>
+  <div id="newsletter" />
 </template>
 
-<style lang="scss">
-h4 {
-  text-align: center;
-}
-.form {
-  text-align: center;
-  &__email {
-    padding: 13px;
-    width: 15rem;
-    border: 1px solid #f0f0f5;
-    margin-right: 13px;
-  }
-  &__submit {
-    border: 1px solid;
-    padding: 10px;
-    outline: none;
-    border-radius: 5px;
-    background: var(--dark-pink);
-    color: #fff;
-    cursor: pointer;
+<script>
+export default {
+  mounted() {
+    let newsletterScript = document.createElement('script')
+    newsletterScript.async = true;
+    newsletterScript.setAttribute('data-uid', '473ee130c5')
+    newsletterScript.setAttribute('src', 'https://sunny-maker-3173.ck.page/473ee130c5/index.js')
+    document.getElementById('newsletter').appendChild(newsletterScript)
+  },
+  metaInfo() {
+    script: [
+      { src: 'https://sunny-maker-3173.ck.page/473ee130c5/index.js', body: true }
+    ]
   }
 }
-@media only screen and (max-width: 650px) {
-  .form {
-    &__submit {
-      background: #111;
-      margin-top: 10px;
-    }
-  }
-}
-</style>
+</script>
