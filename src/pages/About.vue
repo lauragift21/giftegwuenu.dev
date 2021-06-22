3a1a181260e67bebd710825633619dbb78c5fbe3
Gift Egwuenu <laurandidi21@gmail.com>
2021-06-22 11:22:07 +0200

    Add about page

---
+++ src/pages/About.vue
@@ -0,0 +1,51 @@
+<template>
+	<Layout class="w-full">
+		<h1 class="text-4xl font-black mb-6">About Me</h1>
+		<section class="flex content-center">
+			<article class="my-4">
+				<p class="pb-8">
+					Hi, I'm Gift Egwuenu - a frontend developer and content creator originally
+					from Delta, Nigeria but based in Haarlem Netherlands.
+				</p>
+				<p class="pb-8">
+					I have worked in tech and on the web for the past 4 years with experience
+					ranging from Javascript, Vue.js, CSS, Accessibility, Technical Writing and
+					I have worked for companies like Rekuur Solutions, Andela, Ruby Together
+					and Passionate People.
+				</p>
+				<p class="pb-8">
+					I love sharing and contributing to the tech community but it's a big part
+					of my career in tech and I'm consistently engaging and advocating for open
+					and inclusive developer communities.I love sharing my knowledge and do that
+					through content creation on my blog and YouTube channel. I've been
+					nominated as a Media Developer Expert at Cloudinary, Microsoft MVP, an
+					Auth0 Ambassador, GitHub Star, and a Nuxt.js Ambassador.
+				</p>
+				<p class="pb-8">
+					When I'm not coding, you can find me geeking about photography, trying new
+					food recipes, and exploring different ways of creating content that can
+					help people pivot their tech careers.
+				</p>
+				<p class="pb-8">
+					I share a lot of helpful resources for developers and also curate some
+					resources myself. You can check out Educative a platform with helpful
+					courses for developers.
+				</p>
+				<p>You can catch me on Twitter, LinkedIn, Polywork, GitHub</p>
+			</article>
+			<article>
+				<img
+					class="rounded-md"
+					src="https://res.cloudinary.com/lauragift/image/upload/c_scale,q_auto,w_352/v1624353138/4EFA5623-1143-4480-A319-F5389240D87A_hgsluf.jpg"
+				/>
+			</article>
+		</section>
+	</Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
